037cc869499f64471a5f22dce52f45fbb69fe384
MathiasB <37438935+BerlancourtMathias@users.noreply.github.com>
2023-02-06 11:27:23 +0100

    ajout de fonctionnalité reset counter et reset all counters + un peu de css en wip

--- src/App.js
+++ src/App.js
@@ -1,54 +1,85 @@
 import "./App.css";
 import { useState } from "react";
 const App = () => {
-  const nCounter = {
-    valueCounter: 0,
-    numberOfPushingPlus: 0,
-    numberOfPushingMinus: 0,
-    addCounter: 0,
-  };
-  const [displayCounter, setDisplayCounter] = useState([nCounter]);
-  const handleClick = (action) => {
-    const newTab = [...displayCounter];
-    const { valueCounter, numberOfPushingMinus, numberOfPushingPlus } =
-      newTab[0];
+  const [displayCounter, setDisplayCounter] = useState([0]);
+  const handleClick = (action, i) => {
+    let index = i;
     if (action === "minus") {
-      newTab.nCounter.numberOfPushingMinus += 1;
-      valueCounter = numberOfPushingPlus - numberOfPushingMinus;
+      displayCounter[index] -= 1;
+      const newTab = [...displayCounter];
       setDisplayCounter(newTab);
     } else if (action === "plus") {
-      newTab.nCounter.numberOfPushingPlus += 1;
-      valueCounter = numberOfPushingPlus - numberOfPushingMinus;
+      displayCounter[index] += 1;
+      const newTab = [...displayCounter];
       setDisplayCounter(newTab);
-    } else if (action === "Add Counter") {
-      setDisplayCounter(displayCounter.push(newTab));
     }
+    if (action === "Add Counter" && displayCounter.length < 3) {
+      // const newTab = [...displayCounter];
+      const newTab = [...displayCounter];
+      newTab.push(0);
+      setDisplayCounter(newTab);
+    }
+  };
+  const handleResetAll = () => {
+    setDisplayCounter([0]);
+  };
+  const handleResetCounter = (index) => {
+    const newTab = [...displayCounter];
+    newTab[index] = 0;
+    setDisplayCounter(newTab);
   };
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          handleClick("minus");
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          handleClick("plus");
-        }}
-      >
-        +
-      </button>
+      <div className="headerButtons">
+        <button
+          onClick={() => {
+            handleClick("Add Counter");
+          }}
+        >
+          Add Counter
+        </button>
+        <button
+          onClick={() => {
+            handleResetAll();
+          }}
+        >
+          Reset All
+        </button>
+      </div>
+      <div className="mainButtons">
+        {displayCounter.map((elem, index) => (
+          <div className="oneCounter" key={index}>
+            <button
+              onClick={() => {
+                handleClick("minus", index);
+              }}
+            >
+              -
+            </button>
+            <div className="result">{elem}</div>
+            <button
+              onClick={() => {
+                handleClick("plus", index);
+              }}
+            >
+              +
+            </button>
 
-      <button
-        onClick={() => {
-          handleClick("Add Counter");
-        }}
-      >
-        Add Counter
-      </button>
-      <div>RESULT du counter:{displayCounter[0].valueCounter} </div>
+            <button
+              onClick={() => {
+                handleResetCounter(index);
+              }}
+            >
+              Reset Counter
+            </button>
+          </div>
+        ))}
+      </div>
+      <footer>
+        <div>
+          Made with ⚛︎ at <b>Le Reacteur</b> by <b>Mathias</b>
+        </div>
+      </footer>
     </div>
   );
 };
